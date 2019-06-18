6dd895f5084cafa7c68f7d2b6ad36d60cb20dce4
Tobias Hallmayer <tobias.hallmayer@gmail.com>
2019-06-18 16:41:48 +0200

    added qr coderoutes

--- database/qrcode_repo.js
+++ database/qrcode_repo.js
@@ -8,33 +8,37 @@ class QrCodeRepo {
       CREATE TABLE IF NOT EXISTS "QrCode" (
           "ID"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
           "VoucherID"	INTEGER,
-          "Daytime-Active"	INTEGER,
-          "Daytime-Pending"	INTEGER,
-          "Daytime-Over"	INTEGER,
+          "activeDate"	INTEGER,
+          "creationDate"	INTEGER,
+          "overDate"	INTEGER,
           "StationID"	INTEGER,
           "Status"	INTEGER
     )`
-        
+
         return this.dbc.run(sql)
     }
 
     create(voucherId, stationId) {
+        console.log(voucherId);
+        console.log(stationId)
+        var datetime = Date.now();
+        console.log(datetime);
         return this.dbc.run(
-            `INSERT INTO QRCode (VoucherID, Daytime-Pending, StationID, Status)
+            `INSERT INTO QrCode (VoucherID, creationDate, StationID, Status)
             VALUES (?, ?, ?, ?)`,
-            [voucherId, Date.now(), stationId, 0])
+            [voucherId, datetime, stationId, 0])
     }
 
     update(qrCodeId, status) {
         if (status == 1) {
             return this.dbc.run(
-                `UPDATE QRCode SET status = ?, Daytime-Active = ? WHERE id = ?`,
+                `UPDATE QRCode SET status = ?, activeDate = ? WHERE id = ?`,
                 [status, Date.now(), qrCodeId]
             )
         }
         else if (status == 2) {
             return this.dbc.run(
-                `UPDATE QRCode SET status = ?, Daytime-Over = ? WHERE id = ?`,
+                `UPDATE QRCode SET status = ?, overDate = ? WHERE id = ?`,
                 [status, Date.now(), qrCodeId]
             )
         }
